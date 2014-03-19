f130340ebc9bfe404dd1644b9c01305f6be4664b
Pravesh Ramachandran <pravs88@gmail.com>
2014-03-19 13:26:37 -0500

    First RemindMe commit ..Wohhoo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RemindMe::Application.config.secret_key_base = '4664b7cf38f63bcea2d76e2fc0fe47255de791e86862a44e9104e35372e0ae4bfc83bddc84ca83af0a41d987c399857d3d6d304bbba741a9b42904d21b388d02'
